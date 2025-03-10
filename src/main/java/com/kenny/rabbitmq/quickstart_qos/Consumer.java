b64803d218a5f83c9f70d5bd906345cb088f8468
kenny <kenny.kwyiu@gmail.com>
2025-03-10 21:00:27 +0800

    Consumer - qos

---
+++ src/main/java/com/kenny/rabbitmq/quickstart_qos/Consumer.java
@@ -0,0 +1,42 @@
+package com.kenny.rabbitmq.quickstart_qos;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+public class Consumer {
+    public static void main(String[] args) throws Exception {
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+
+        connectionFactory.setHost("192.168.74.144");
+        connectionFactory.setPort(5672);
+        connectionFactory.setVirtualHost("/");
+        connectionFactory.setUsername("admin");
+        connectionFactory.setPassword("password");
+        connectionFactory.setRequestedHeartbeat(10000);
+
+        try (Connection connection = connectionFactory.newConnection();
+             Channel channel = connection.createChannel()) {
+
+            String exchangeName = "test_return_exchange";
+            String routingKey = "return.#";
+            String queueName = "test_return_queue";
+
+
+            channel.exchangeDeclare(exchangeName, "topic", true, false, null);
+            channel.queueDeclare(queueName, true, false, false, null);
+            channel.queueBind(queueName, exchangeName, routingKey);
+
+            DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+                String message = new String(delivery.getBody());
+                System.out.println("Consumer Received: " + message);
+            };
+
+            channel.basicQos(0,2,false);
+            channel.basicConsume(queueName, false, new MyConsumer(channel));
+
+            Thread.sleep(Long.MAX_VALUE);
+        }
+    }
+}
